f626489e0185b712dc5f1e52347e15cf37db4ef3
NikolasDaltro <nikolasdsantos@gmail.com>
2022-11-30 12:31:28 -0300

    aplicando Binding

--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-crud',
   templateUrl: './product-crud.component.html',
   styleUrls: ['./product-crud.component.css']
 })
-export class ProductCrudComponent {
+export class ProductCrudComponent implements OnInit {
 
+  propLegal = "qualquer"
+
+constructor(){}
+
+ngOnInit(): void {
+  
+}
+fazerAlgo(): void {
+  console.log('Fazendo Algo')
+}
 }
