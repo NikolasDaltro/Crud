259996bbbf6a5d54575bcfbcfc0b22093b350768
NikolasDaltro <nikolasdsantos@gmail.com>
2022-12-01 21:44:47 -0300

    aplicando estilo ao formulario

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -20,8 +20,13 @@ import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule} from '@angular/material/snack-bar';
-
 import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
+
+
 
 
 @NgModule({
@@ -46,7 +51,11 @@ import { HttpClientModule } from '@angular/common/http';
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
+
 
 
   ],
